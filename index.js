816169bdd34efeb8b0847db0f28cadf1a34f986b
Bkroland19 <rkbukenya@cis.mak.ac.ug>
2021-08-26 09:40:13 +0300

    new commit

--- index.js
+++ index.js
@@ -6,34 +6,46 @@ const JsonSchema = mongoose.model("JsonSchema");
 
 //mongoose connection
 mongoose.connect(process.env.DATABASE, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
 });
 
 mongoose.connection
-  .on("open", () => {
-    console.log("Mongoose connection open");
-  })
-  .on("error", (err) => {
-    console.log(`Connection error: ${err.message}`);
-  });
+	.on("open", () => {
+		console.log("Mongoose connection open");
+	})
+	.on("error", (err) => {
+		console.log(`Connection error: ${err.message}`);
+	});
+
+//
 
 // async await
 (async () => {
-  var rss = await parse("https://blog.ethereum.org/feed.xml");
-
-  // console.log(JSON.stringify(rss, null, 3));
+	// var rss = await parse("https://blog.ethereum.org/feed.xml");
 })();
 // Promise
+const links = [
+	"https://blog.ethereum.org/feed.xml",
+	"https://careeralley.com/feed/",
+];
 
-parse("https://blog.ethereum.org/feed.xml").then((rss) => {
-  // console.log(JSON.stringify(rss, null, 3));
-  var jsonSchema = new JsonSchema(JSON.stringify(rss, null, 3));
-
-  // JsonSchema.insert()
-  db.collection.save(jsonSchema).then(() => {
-    res.send("Json records saved to Database");
-  });
+links.map((link) => {
+	parse(link).then(async (rss) => {
+		// console.log(JSON.stringify(link, null, 3));
+		var rss = await parse(link);
+		console.log(rss);
+		// link.items?.map((object) => {
+		// 	var jsonSchema = new JsonSchema(object);
+		// 	jsonSchema.save();
+		// 	return "data sent to the db";
+		// });
+		//console.log(link.items);
+		// // JsonSchema.insert()
+		// jsonSchema.save().then(() => {
+		// 	res.send("Json records saved to Database");
+		// });
+	});
 });
